92d244205a8848570ddc22537282356a9dd32223
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-11-10 03:59:49 +0200

    export more methods from connection

--- src/useWebmesh.ts
+++ src/useWebmesh.ts
@@ -1,5 +1,7 @@
 import { Ref, ref, toValue, watchEffect } from 'vue';
 import { DaemonClient, WebmeshOptions } from './options';
+import { ConnectResponse } from '@webmeshproject/api/v1/app_pb';
+import { MeshNodes } from '@webmeshproject/api/utils/rpcdb';
 
 // WebmeshContext is the context for using Webmesh.
 export interface WebmeshContext {
@@ -26,8 +28,8 @@ export function useWebmesh(
         return new Promise((resolve, reject) => {
             client.value
                 .connect({ id: connectionID })
-                .then((res) => {
-                    resolve(new Connection(client.value, res.id));
+                .then((res: ConnectResponse) => {
+                    resolve(new Connection(client.value, res));
                 })
                 .catch((err: Error) => {
                     reject(err);
@@ -73,6 +75,73 @@ export class Connections {
 export class Connection {
     constructor(
         private client: DaemonClient,
-        private connectionID: string,
+        private connectionDetails: ConnectResponse,
     ) {}
+
+    // id returns the ID of the connection.
+    public get id(): string {
+        return this.connectionDetails.id;
+    }
+
+    // ipv4Address returns the IPv4 address of the connection.
+    public get ipv4Address(): string {
+        return this.connectionDetails.ipv4Address;
+    }
+
+    // ipv6Address returns the IPv6 address of the connection.
+    public get ipv6Address(): string {
+        return this.connectionDetails.ipv6Address;
+    }
+
+    // ipv4Network returns the IPv4 network of the connection.
+    public get ipv4Network(): string {
+        return this.connectionDetails.ipv4Network;
+    }
+
+    // ipv6Network returns the IPv6 network of the connection.
+    public get ipv6Network(): string {
+        return this.connectionDetails.ipv6Network;
+    }
+
+    // domain returns the domain of the connection.
+    public get domain(): string {
+        return this.connectionDetails.meshDomain;
+    }
+
+    // fqdn returns the fully qualified domain name of the connection.
+    public get fqdn(): string {
+        return this.id + '.' + this.domain;
+    }
+
+    // peers returns an interface for querying the peers of this connection.
+    public get peers(): MeshNodes {
+        return new MeshNodes(this.client, this.id);
+    }
+
+    // connect connects to the connection.
+    public connect(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client
+                .connect({ id: this.id })
+                .then((res: ConnectResponse) => {
+                    this.connectionDetails = res;
+                    resolve();
+                })
+                .catch((err: Error) => {
+                    reject(err);
+                });
+        });
+    }
+
+    // disconnect disconnects from the connection.
+    public disconnect(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client
+                .disconnect({ id: this.id })
+                .then(() => resolve())
+                .catch((err: Error) => {
+                    reject(err);
+                });
+        });
+    }
 }
