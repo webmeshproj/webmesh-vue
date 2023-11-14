ec581ce8aadae472b44c295a19d4f2494f9099c4
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-11-14 12:42:28 +0200

    re-export types used by the hook

--- index.ts
+++ index.ts
@@ -1,3 +1,26 @@
+import {
+    Network,
+    NetworkParameters,
+    Parameters,
+    Metrics,
+} from '@webmeshproject/api/utils/networks';
+import {
+    DaemonClient,
+    Options,
+    DefaultNamespace,
+    DefaultDaemonAddress,
+} from '@webmeshproject/api/utils/daemon';
 import { DaemonOptions, Context, useWebmesh } from './src/useWebmesh';
-export type { Context };
-export { DaemonOptions, useWebmesh };
+
+export type { DaemonClient, Metrics, NetworkParameters };
+
+export {
+    DefaultNamespace,
+    DefaultDaemonAddress,
+    Context,
+    DaemonOptions,
+    Options,
+    Parameters,
+    Network,
+    useWebmesh,
+};
