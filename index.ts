8d7a9edea024a97e4ac0078649544684416548c0
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-11-15 11:37:31 +0200

    export more helper types from core api

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
-import { DaemonStatus } from '@webmeshproject/api/v1/app_pb';
+import { Feature } from '@webmeshproject/api/v1/node_pb';
+import { AppDaemon } from '@webmeshproject/api/v1/app_connect';
+import {
+    NetworkAuthMethod,
+    ConnectionParameters_AddrType as AddrType,
+    ConnectionParameters_AuthHeader as AuthHeader,
+    MeshConnBootstrap_DefaultNetworkACL as DefaultNetworkACL,
+    DaemonConnStatus as ConnectionStatus,
+    DaemonStatus,
+} from '@webmeshproject/api/v1/app_pb';
+
 import {
     Defaults,
     Network,
@@ -12,11 +22,22 @@ import {
     DefaultNamespace,
     DefaultDaemonAddress,
 } from '@webmeshproject/api/utils/daemon';
+
 import { DaemonOptions, Context, useWebmesh } from './src/useWebmesh';
 
-export type { Parameters, Context, DaemonClient, Metrics, NetworkParameters };
+export type {
+    AddrType,
+    AuthHeader,
+    ConnectionStatus,
+    DefaultNetworkACL,
+    Feature,
+    NetworkAuthMethod,
+};
+
+export type { Context, DaemonClient, Metrics, NetworkParameters, Parameters };
 
 export {
+    AppDaemon,
     DefaultNamespace,
     DefaultDaemonAddress,
     DaemonOptions,
